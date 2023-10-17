a2edfaba61fd5d59de4d00ecdf1dab87b5076f2b
Abhaya <shankarabhaya9@gmail.com>
2023-10-18 01:08:15 +0530

    theme toggler - how to use

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,26 @@
 import { NavbarLinks } from "@/constants";
 import Link from "next/link";
 import { ModeToggle } from "./ThemeToggler";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Montserrat({
+  subsets: ["latin"],
+  weight: ["500"],
+});
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center justify-between p-4 py-6 ">
-      <h1>Fitness Hub</h1>
-      <div className="flex space-x-6 ">
+    <div className="flex items-center justify-between p-4 py-6 border-b-2  ">
+      <h1 className="font-bold text-2xl tracking-widest">Fitness Hub</h1>
+      <div className="flex space-x-6  ">
         {NavbarLinks.map((item) => (
           <Link
             key={item.id}
-            className="hover:text-red-500 transition-all delay-75"
+            className={cn(
+              "transition-all delay-75 text-[17px] dark:text-gray-400 dark:hover:text-white font-medium",
+              font.className
+            )}
             href={item.href}
           >
             {item.label}
---
+++ todo.md
@@ -0,0 +1,12 @@
+List of things you are supposed to fix
+
+1. how to remove **HYDRATION_ERROR**.
+
+List of things you are supposed to do - *learning perspective*
+
+1. How does Clerk JS works.
+2. How to use dark and light theme designs - 
+   ```css
+   dark:text-gray-400 dark:hover:text-white
+   ```
+   Similarly use dark and light theme in CSS.
