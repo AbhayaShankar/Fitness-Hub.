e487c32d160b4b70b8b6d856a787791887c53f80
Abhaya <shankarabhaya9@gmail.com>
2023-10-26 13:11:24 +0530

    Pricing plan started

--- app/(routes)/(otherRoutes)/pricing/page.tsx
+++ app/(routes)/(otherRoutes)/pricing/page.tsx
@@ -1,3 +1,5 @@
+import PricingCard from "@/components/PricingCard";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const PricingPage = () => {
@@ -6,6 +8,18 @@ const PricingPage = () => {
       <h1 className="absolute top-[260px] left-[44%] text-[30px] tracking-wide font-semibold capitalize text-white dark:text-white">
         Pricing
       </h1>
+      <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center gap-10 p-10">
+        <div>
+          <h2>Pick your plan</h2>
+          <div>
+            <h3>Monthly Billing</h3>
+            <h3>Yearly Billing</h3>
+          </div>
+        </div>
+        <PricingCard planType="Beginners Plan" amount={0} />
+        <PricingCard planType="Intermediate Plan" amount={30} />
+        <PricingCard planType="Advance Plan" amount={50} />
+      </div>
     </div>
   );
 };
---
+++ components/PricingCard.tsx
@@ -0,0 +1,27 @@
+import { Button } from "./ui/button";
+
+interface PricingCardProps {
+  planType: string;
+  amount: number;
+}
+
+const PricingCard = ({ planType, amount }: PricingCardProps) => {
+  return (
+    <div className=" min-w-[220px]  rounded-lg p-5 text-center hover:bg-[#0c0a09] hover:dark:bg-white/90 transition-all duration-100 ease-in group hover:scale-[1.05]">
+      <h2 className="text-base font-medium mb-5 group-hover:text-white dark:group-hover:text-[#0c0a09]">
+        {planType}
+      </h2>
+      <h1 className="text-5xl font-bold mb-2 group-hover:text-white dark:group-hover:text-[#0c0a09]">
+        ${amount}
+      </h1>
+      <p className="text-xs font-medium text-muted-foreground mb-8 group-hover:text-white dark:group-hover:text-[#0c0a09]">
+        Per Month
+      </p>
+      <Button variant={"outline"} size={"sm"} className="rounded-full">
+        Get Started
+      </Button>
+    </div>
+  );
+};
+
+export default PricingCard;
