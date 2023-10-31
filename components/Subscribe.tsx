7e8d7e49e22f86a64a9abacae84793f0d2ee260e
Abhaya <shankarabhaya9@gmail.com>
2023-10-31 23:41:32 +0530

    subscribe sheet upadte

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Montserrat({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
 
 const SubscribeComp = () => {
   const [mounted, setMounted] = useState(false);
@@ -13,7 +22,30 @@ const SubscribeComp = () => {
     return null;
   }
 
-  return <div className="z-50 ">SubscribeComp</div>;
+  return (
+    <div className={cn("z-50", font.className)}>
+      <h1 className="uppercase text-lg lg:text-2xl font-semibold text-center mt-10">
+        Subscribe to Fitness
+      </h1>
+      <div className="relative">
+        <Image
+          src={"/assets/subscribe-cover.jpg"}
+          alt="subscribe-cover-img"
+          width={500}
+          height={500}
+          className="w-full h-[380px] object-cover p-2 rounded-2xl"
+        />
+        <div className="w-[320px] absolute bottom-5 left-8 bg-[#3333337b] rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 p-4">
+          <h1 className=" text-2xl font-semibold text-white">
+            Struggle is Crucial & Essential to progress
+          </h1>
+          <p className="text-sm dark:text-muted-foreground text-white/50">
+            Abhaya Shankar, Personal Trainer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubscribeComp;
