5422c0adde7023bbce1fccc0c6b790be2d362ad2
Abhaya <shankarabhaya9@gmail.com>
2023-11-26 22:13:51 +0530

    form validation completed

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -5,7 +5,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "./ui/card";
@@ -23,16 +22,16 @@ import { Button } from "./ui/button";
 import { ALL_CLASSES } from "@/constants";
 import { Label } from "./ui/label";
 
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-
-const validOptions = z.enum(ALL_CLASSES.map((item) => item.name));
+import { Check } from "lucide-react";
 
 type FormData = {
   name: string;
   email: string;
   comments: string;
+  selectField: string;
 };
 
 const schema = z.object({
@@ -41,21 +40,23 @@ const schema = z.object({
     .string()
     .min(1, { message: "Please enter email" })
     .email("This is not a valid email."),
-  //   select: validOptions,
-  comments: z.string().min(1),
+  selectField: z.string().min(1, { message: "Please select a class" }),
+  comments: z.string().min(1, { message: "Please add comments" }),
 });
 
 const ContactForm = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
     reset,
-  } = useForm({
+    setValue,
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
-  const onSubmit = (data: FormData) => {
+  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
     console.log(data);
     reset();
   };
@@ -86,7 +87,7 @@ const ContactForm = () => {
                   placeholder="Full Name*"
                   {...register("name")}
                 />
-                {errors.name?.message && (
+                {typeof errors.name?.message === "string" && (
                   <p className="text-sm text-red-500 pl-1 space-y-0">
                     {errors.name?.message}
                   </p>
@@ -98,28 +99,91 @@ const ContactForm = () => {
                   className="placeholder:tracking-wide"
                   {...register("email")}
                 />
-                {errors.email?.message && (
+                {typeof errors.email?.message === "string" && (
                   <p className="text-sm text-red-500 pl-1">
                     {errors.email?.message}
                   </p>
                 )}
               </div>
               <div className="flex flex-col space-y-4">
-                <Label htmlFor="Class" className="pl-1">
+                <Label htmlFor="selectField" className="pl-1">
                   Classes
                 </Label>
-                <Select>
-                  <SelectTrigger id="classes">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    {ALL_CLASSES.map((item) => (
-                      <SelectItem value={item.name} key={item.id}>
-                        {item.name}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+                <Controller
+                  name="selectField"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "This field is required" }}
+                  render={({ field, fieldState }) => (
+                    // <div>
+                    //   <Select
+                    //     value={field.value || "Select Class"}
+                    //     onChange={(e) =>
+                    //       setValue("selectField", e.target.value)
+                    //     }
+                    //   >
+                    //     <SelectTrigger id="classes">
+                    //       <SelectValue>
+                    //         {field.value ? field.value : "Select"}
+                    //       </SelectValue>
+                    //     </SelectTrigger>
+                    //     <SelectContent position="popper">
+                    //       {ALL_CLASSES.map((item) => (
+                    //         <SelectItem
+                    //           value={item.name}
+                    //           key={item.id}
+                    //           onSelect={() =>
+                    //             setValue("selectField", item.name)
+                    //           }
+                    //         >
+                    //           {item.name}
+                    //         </SelectItem>
+                    //       ))}
+                    //     </SelectContent>
+                    //   </Select>
+                    //   {fieldState && fieldState.error && (
+                    //     <p className="text-sm text-red-500 pl-1 space-y-0">
+                    //       {fieldState.error.message}
+                    //     </p>
+                    //   )}
+                    // </div>
+                    <div>
+                      <select
+                        value={field.value || ""}
+                        onChange={(e) =>
+                          setValue("selectField", e.target.value)
+                        }
+                        className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                      >
+                        <option
+                          className="text-muted-foreground"
+                          value=""
+                          disabled
+                        >
+                          Select
+                        </option>
+                        {ALL_CLASSES.map((item) => (
+                          <option
+                            className="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 mt-5"
+                            value={item.name}
+                            key={item.id}
+                          >
+                            {/* <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+                              {field.value && <Check className="h-4 w-4" />}
+                            </span> */}
+                            {item.name}
+                          </option>
+                        ))}
+                      </select>
+                      {fieldState && fieldState.error && (
+                        <p className="text-sm text-red-500 pl-1 space-y-0">
+                          {fieldState.error.message}
+                        </p>
+                      )}
+                    </div>
+                  )}
+                />
+
                 <div>
                   <Textarea
                     className="placeholder:tracking-wide"
@@ -127,7 +191,7 @@ const ContactForm = () => {
                     id="message"
                     {...register("comments")}
                   />
-                  {errors.comments?.message && (
+                  {typeof errors.comments?.message === "string" && (
                     <p className="text-sm text-red-500 pl-1 pt-0 mt-0 space-y-2">
                       {errors.comments?.message}
                     </p>
