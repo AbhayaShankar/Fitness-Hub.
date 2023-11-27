6df3162c98d8acd5bf34d36aee5a008bc71963a2
Abhaya <shankarabhaya9@gmail.com>
2023-11-28 00:24:37 +0530

    exercise json update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import Head from "next/head";
 
 const font = Montserrat({
   subsets: ["latin"],
@@ -23,10 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <Head>
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/logo.png" sizes="any" />
-      </Head>
+      </head>
       <html lang="en">
         <body className={cn("h-full", font.className)}>
           <ThemeProvider
