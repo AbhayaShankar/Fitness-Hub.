4c31ee20281d69efc7f9dddb54ba404233a08cd8
Abhaya <shankarabhaya9@gmail.com>
2023-11-30 03:38:54 +0530

    buug check

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,4 @@
-import { AboutBanner2 } from "@/components/AboutBanner";
+// import { AboutBanner2 } from "@/components/AboutBanner";
 import BMIComponent from "@/components/BMI";
 import ClassGrid from "@/components/ClassGrid";
 import GalleryGrid from "@/components/GalleryGrid";
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="flex  space-y-10 lg:pb-20 flex-col bg-[url(/assets/bg/bg-2.jpg)] dark:bg-[url(/assets/bg/dark-bg2.jpg)] pb-8">
       <LandingHero />
       <StagesGrid />
-      <AboutBanner2 />
+      {/* <AboutBanner2 /> */}
       <Trainers />
       <ClassGrid />
       <GymCards />
