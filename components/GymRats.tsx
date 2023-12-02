69613f0e40b812ca8f8ca2c1b4f60256f2f3452d
Abhaya <shankarabhaya9@gmail.com>
2023-12-02 13:58:00 +0530

    card design 75% done

--- components/GymRats.tsx
+++ components/GymRats.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { Instagram, Twitter } from "lucide-react";
 
 const GymRats = () => {
   const handleClick = () => {
@@ -25,24 +27,89 @@ const GymRats = () => {
                       2 <sup>nd</sup>
                     </h3>
                     <span className="text-[#FFC000] text-xl">
-                      &#9733; &#9733; &#9733;
+                      &#9733; &#9733; &#9734;
                     </span>
                   </div>
                   <div className="text-xl uppercase font-semibold -mt-2 tracking-wider">
                     ABHAYA SHANKAR
                   </div>
                 </div>
+                <div className="uppercase text-2xl font-medium tracking-widest">
+                  Powerlifting
+                </div>
+                <div className="tracking-wide">Dhanbad, Jharkhand</div>
               </div>
             </div>
           </div>
           {/* After Flip */}
           <div className="flip-box-back rounded-md border-[1px] border-muted-foreground dark:border-muted text-center bg-[url('/assets/test.jpg')]">
-            <div className="inner text-white">
-              <h3 className="flip-box-header">Custom Domains</h3>
-              <p>A short sentence describing this callout is.</p>
-              <Button onClick={handleClick} className="flip-box-button">
-                Learn More
-              </Button>
+            <div className="inner-back text-white text-left">
+              <ul className="text-lg pl-2">
+                <li className="list-image-[url('/assets/custom-list.png')] list-outside">
+                  2<sup>nd</sup> position at xyz Powerlifting competition.
+                </li>
+                <br />
+                <li className="list-image-[url('/assets/custom-list.png')] list-outside">
+                  Max Bench - 105 kgs
+                </li>
+                <li className="list-image-[url('/assets/custom-list.png')] list-outside">
+                  Max Deadlift - 190 kgs
+                </li>
+                <li className="list-image-[url('/assets/custom-list.png')] list-outside">
+                  Max Squats - 125 kgs
+                </li>
+              </ul>
+              <div>
+                <h3 className="text-center capitalize text-lg mt-8 mb-4">
+                  Follow for More ...
+                </h3>
+                <div className="flex items-center gap-5 justify-center">
+                  <Link
+                    className="group"
+                    href={"https://www.abhayashankar.com"}
+                    target="_blank"
+                  >
+                    <Instagram
+                      color="#5f5f5f"
+                      strokeWidth={2}
+                      className="w-10 h-10 group-hover:stroke-[#ce032c] transition-all ease-in duration-150 p-[6px] border-[1px] rounded-full group-hover:border-[#ce032c]"
+                    />
+                  </Link>
+                  <Link
+                    className="group"
+                    href={"https://www.abhayashankar.com"}
+                    target="_blank"
+                  >
+                    <Instagram
+                      color="#5f5f5f"
+                      strokeWidth={2}
+                      className="w-10 h-10 group-hover:stroke-[#ce032c] transition-all ease-in duration-150 p-[6px] border-[1px] rounded-full group-hover:border-[#ce032c]"
+                    />
+                  </Link>
+                  <Link
+                    className="group"
+                    href={"https://www.abhayashankar.com"}
+                    target="_blank"
+                  >
+                    <Instagram
+                      color="#5f5f5f"
+                      strokeWidth={2}
+                      className="w-10 h-10 group-hover:stroke-[#ce032c] transition-all ease-in duration-150 p-[6px] border-[1px] rounded-full group-hover:border-[#ce032c]"
+                    />
+                  </Link>
+                  <Link
+                    className="group"
+                    href={"https://www.abhayashankar.com"}
+                    target="_blank"
+                  >
+                    <Instagram
+                      color="#5f5f5f"
+                      strokeWidth={2}
+                      className="w-10 h-10 group-hover:stroke-[#ce032c] transition-all ease-in duration-150 p-[6px] border-[1px] rounded-full group-hover:border-[#ce032c]"
+                    />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
